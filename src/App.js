7a47b77a3d13249e184384978de4299702638322
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-24 02:30:29 +0600

    check tailwind and added settings component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App</h1>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
