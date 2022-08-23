1cbf2d4dbda50edc43a36905dea1568517640f30
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-24 03:19:49 +0600

    added imports for app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
 import "./App.css";
 
 const App = () => {
