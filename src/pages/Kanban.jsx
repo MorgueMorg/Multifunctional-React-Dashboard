7f8349c39fd0bbf551d0de990942ba95fa1c1527
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-25 02:13:07 +0600

    added kanban page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
 
-const Kanban = () => {
-  return (
-    <div>Kanban</div>
-  )
-}
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 
-export default Kanban
+const Kanban = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Kanban" />
+    <KanbanComponent
+      id="kanban"
+      keyField="Status"
+      dataSource={kanbanData}
+      cardSettings={{ contentField: "Summary", headerField: "Id" }}
+    >
+      <ColumnsDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {kanbanGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective>
+    </KanbanComponent>
+  </div>
+);
+
+export default Kanban;
