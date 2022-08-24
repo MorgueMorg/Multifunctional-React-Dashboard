bf6b17285453c13126f7a65d60c69ba70bbe31c2
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-25 02:37:59 +0600

    added editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
 
-const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
-export default Editor
+const Editor = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Editor" />
+    <RichTextEditorComponent>
+      <EditorData />
+      <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+    </RichTextEditorComponent>
+  </div>
+);
+export default Editor;
