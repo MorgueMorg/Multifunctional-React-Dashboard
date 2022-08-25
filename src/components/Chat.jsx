c27a2cf71b9f41f102743e6f7b5cb1631c15ce92
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-26 02:21:46 +0600

    added chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,71 +1,68 @@
 import React from "react";
 import { MdOutlineCancel } from "react-icons/md";
-import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
-import { useStateContext } from "../contexts/ContextProvider";
-import { cartData } from "../data/dummy";
 import { Button } from ".";
+import { chatData } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
-const Cart = () => {
+const Chat = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
-      <div className="float-right h-screen  duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8">
-        <div className="flex justify-between items-center">
-          <p className="font-semibold text-lg">Shopping Cart</p>
-          <Button
-            icon={<MdOutlineCancel />}
-            color="rgb(153, 171, 180)"
-            bgHoverColor="light-gray"
-            size="2xl"
-            borderRadius="50%"
-          />
+    <div className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3">
+          <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
+          <button
+            type="button"
+            className="text-white  text-xs rounded p-1 px-2 bg-orange"
+          >
+            5 New
+          </button>
         </div>
-        {cartData?.map((item, index) => (
-          <div key={index}>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
+      </div>
+      <div className="mt-5 ">
+        {chatData?.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 border-b-1 border-color p-3 leading-8 cursor-pointer"
+          >
+            <div className="relative">
+              <img
+                className="rounded-full h-10 w-10"
+                src={item.image}
+                alt={item.message}
+              />
+              <span
+                style={{ background: item.dotColor }}
+                className="absolute inline-flex rounded-full h-2 w-2 right-0 -top-1"
+              />
+            </div>
             <div>
-              <div className="flex items-center   leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4">
-                <img className="rounded-lg h-80 w-24" src={item.image} alt="" />
-                <div>
-                  <p className="font-semibold ">{item.name}</p>
-                  <p className="text-gray-600 dark:text-gray-400 text-sm font-semibold">
-                    {item.category}
-                  </p>
-                  <div className="flex gap-4 mt-2 items-center">
-                    <p className="font-semibold text-lg">{item.price}</p>
-                    <div className="flex items-center border-1 border-r-0 border-color rounded">
-                      <p className="p-2 border-r-1 dark:border-gray-600 border-color text-red-600 ">
-                        <AiOutlineMinus />
-                      </p>
-                      <p className="p-2 border-r-1 border-color dark:border-gray-600 text-green-600">
-                        0
-                      </p>
-                      <p className="p-2 border-r-1 border-color dark:border-gray-600 text-green-600">
-                        <AiOutlinePlus />
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <p className="font-semibold dark:text-gray-200 ">
+                {item.message}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400 text-sm">
+                {item.desc}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400 text-xs">
+                {item.time}
+              </p>
             </div>
           </div>
         ))}
-        <div className="mt-3 mb-3">
-          <div className="flex justify-between items-center">
-            <p className="text-gray-500 dark:text-gray-200">Sub Total</p>
-            <p className="font-semibold">$890</p>
-          </div>
-          <div className="flex justify-between items-center mt-3">
-            <p className="text-gray-500 dark:text-gray-200">Total</p>
-            <p className="font-semibold">$890</p>
-          </div>
-        </div>
         <div className="mt-5">
           <Button
             color="white"
             bgColor={currentColor}
-            text="Place Order"
+            text="See all messages"
             borderRadius="10px"
             width="full"
           />
@@ -75,4 +72,4 @@ const Cart = () => {
   );
 };
 
-export default Cart;
+export default Chat;
