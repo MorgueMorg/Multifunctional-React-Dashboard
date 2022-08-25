72a05c9abf63a5c76a35967feb63ac6f17d2f9b5
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-26 03:07:38 +0600

    added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = ({ category, title }) => (
+  <div className=" mb-10">
+    <p className="text-lg text-gray-400">{category}</p>
+    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+      {title}
+    </p>
+  </div>
+);
 
-export default Header
+export default Header;
