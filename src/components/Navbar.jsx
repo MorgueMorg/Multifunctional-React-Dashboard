fdcc2b479331e42259d171cae011f90a9b109499
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-26 03:16:12 +0600

    updated navbar :shipit:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ const Navbar = () => {
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{" "}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Michael
+                Morg
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
