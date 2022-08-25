4c4fca740cf1a1bcf2f920705b1f3bdbdeb109da
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-25 22:41:40 +0600

    added line page

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Line = () => {
-  return (
-    <div>Line</div>
-  )
-}
+import { ChartsHeader, LineChart } from "../../components";
 
-export default Line
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
+    </div>
+  </div>
+);
+
+export default Line;
